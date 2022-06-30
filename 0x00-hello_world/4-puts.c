7c5beab9315f3ef96ec4f84e362bc14ca91e8e7d
Adeniyi Oluwajana <adeniyi.oluwajana@gmail.com>
2022-06-30 15:59:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,14 @@
 #include<stdio.h>
-
 /**
- *
- * * main - Entry point
- *
- * *
- *
- * *Description: print "Programming is like building a multilingual puzzle"
- *
- * *
- *
- * * Return: returns value 0
- *
- * */
-
+* main - Entry point
+*
+*Description: print "Programming is like building a multilingual puzzle"
+*
+* Return: returns value 0
+*/
 int main(void)
-
 {
-
-	char str[52] = "\"Programming is like building a multilingual puzzle";
-
-	puts(str);
-
-	return (0);
-
+char str[52] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
