b105b6e2f27805cf2a312b4a1a804034ec4769d3
Adeniyi Oluwajana <adeniyi.oluwajana@gmail.com>
2022-07-06 12:17:57 -0700

    100times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,10 @@
 void print_times_table(int n)
 {
 	int a = 0, rep, b;
-	
+
 	if (n < 0 || n > 15)
 		return;
+
 	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
@@ -28,11 +29,8 @@ void print_times_table(int n)
 			{
 				_putchar (' ');
 				_putchar('0' + rep / 10);
+				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 10);
 				_putchar('0' + rep % 10);
 			}
@@ -40,9 +38,7 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar('\n');
+				a++;
 			}
-		}
-		_putchar('\n');
-		a++;
-	}
-}
+}	
