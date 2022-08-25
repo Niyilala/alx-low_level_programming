4b64ecc849befe0dfa9f77828508847a41503061
Adeniyi Oluwajana <adeniyi.oluwajana@gmail.com>
2022-08-24 20:52:51 -0700

    Write a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -53,13 +53,13 @@ void check_elf(unsigned char *e_ident)
 void print_magic(unsigned char *e_ident)
 {
 	int index;
-	
+
 	printf("  Magic:   ");
 
 	for (index = 0; index < EI_NIDENT; index++)
 	{
 		printf("%02x", e_ident[index]);
-		
+
 		if (index == EI_NIDENT - 1)
 			printf("\n");
 		else
@@ -123,7 +123,7 @@ void print_version(unsigned char *e_ident)
 {
 	printf("  Version:                           %d",
 			e_ident[EI_VERSION]);
-	
+
 	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
@@ -232,17 +232,17 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 	printf("  Entry point address:               ");
-	
+
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
 			((e_entry >> 8) & 0xFF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
-	
+
 	if (e_ident[EI_CLASS] == ELFCLASS32)
 		printf("%#x\n", (unsigned int)e_entry);
-	
+
 	else
 		printf("%#lx\n", e_entry);
 }
