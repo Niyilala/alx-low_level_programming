3f58ffcfd5c59beafc6df680bed4bcde2c95e70b
Niyilala <adeniyi.oluwajana@gmail.com>
2022-10-05 21:35:37 -0700

    task 0

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
-*_isupper - function that verifies if a  character is uppercase or not
-*@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
+* _isupper - check for upper case character
+* @c: integer to be used for the function
+* Return: 1 if upper case but 0 if not
 */
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
-*_memset - The _memset() function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
+* *_memset - memory filled with a constant byte
+* @s: pointer
+* @b: constant byte
+* @n: size of buffer to fill
+* Return: pointer to s
 */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		s[n - 1] = b;
-		n--;
+		s[i] = b;
+		i++;
 	}
 	return (s);
 }
