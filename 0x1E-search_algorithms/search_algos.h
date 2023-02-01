cba442114330369a92197cfbca2a98619b64584c
Adeniyi <adeniyi.oluwajana@gmail.com>
2023-02-01 21:24:16 +0000

    search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linear searches
+ * binary serach
+ *
+ */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
